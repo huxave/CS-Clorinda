3e5d39dd99cfdc9f3468a838ab37efa11a4e9d56
huxave <111394174+huxave@users.noreply.github.com>
2022-08-17 22:23:37 -0300

    actualizacion
    Se rectificó la variable teléfono, no configurada como tal pero si incluída como texto plano en action posterior al submit. Numero de celular de Yanina Rolón

--- js/form.js
+++ js/form.js
@@ -1,9 +1,7 @@
 document.querySelector("#submit").addEventListener("click", e => {
   e.preventDefault();
 
-  //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-  // let telefono = "5493704404352";
-
+  
   let cliente = document.querySelector("#cliente").value;
   let celular = document.querySelector("#celular").value;
   let motivo = document.querySelector("#motivo").value;
@@ -15,7 +13,7 @@ document.querySelector("#submit").addEventListener("click", e => {
   resp.classList.remove("fail");
   resp.classList.remove("send");
 
-  let url = `https://api.whatsapp.com/send?phone=+5493704369273&text= Hola!, soy ${cliente} y quiero reservar un lugar para el día ${fecha} a las ${horario}, para ${motivo}, somos ${cantidad} personas,  mi teléfono de contacto es ${celular}.`
+  let url = `https://api.whatsapp.com/send?phone=+5493718608462&text= Hola!, soy ${cliente} y quiero reservar un lugar para el día ${fecha} a las ${horario}, para ${motivo}, somos ${cantidad} personas,  mi teléfono de contacto es ${celular}.`
 
 
   if (cliente === "" || celular === "" || motivo === "" || cantidad === "" || fecha === "" || horario === "") {
